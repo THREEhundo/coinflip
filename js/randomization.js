2caa3f36a9cafc58b29561d039d16ee36ea70659
THREEhundo <samuelbaik87@gmail.com>
2022-05-24 15:50:44 -0700

    added randomNum func

---
+++ js/randomization.js
@@ -0,0 +1,4 @@
+function randomNum() {
+	const flip = Math.random()
+	return flip > 0.5 ? 'Heads' : 'Tails'
+}
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const http = require('http')
+const randomization = require('./js/randomization.js')
 
-const server = http.createServer((req, res) => {})
+const server = http.createServer((req, res) => {
+	res.statusCode = 200
+	res.setHeader('Content-Type', 'application/javascript')
+	res.write('<h1>Hello World</h1>')
+	res.end()
+})
 
 const PORT = process.env.PORT || 5000
 
